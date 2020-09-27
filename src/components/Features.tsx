5f3dcf7481639a620770ef0eecada1856bca5ffa
Igboanugwo Collins <munisco12@gmail.com>
2020-09-28 00:59:40 +0100

    Adding icons from react-icons is awesome

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { ImSphere} from 'react-icons/im'
 
 function Features() {
     return (
         <div className='features'>
             <div className="feature">
-                <svg className='feature__icon'>
-                    <use xlinkHref="img/sprite.svg#icon-global"/>
-                </svg>
+                <div className="feature__icon">
+                    <ImSphere/>
+                </div>
                 <h4 className="header-4">World's best homes</h4>
                 <p className="feature__text">Quisquam distinctio id rerum neque molestiae, necessitatibus a? Architecto nesciunt saepe ratione rem, laboriosam laborum perspiciatis?</p>
             </div>
-            <div className="feature">feature 2 </div>
+            <div className="feature">feature 2</div>
             <div className="feature">feature 3</div>
             <div className="feature">feature 4</div>
             <div className="feature">feature 5</div>
