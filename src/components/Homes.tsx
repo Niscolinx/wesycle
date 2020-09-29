548661feb38a8ae15e953d068042a31014ed7a52
Igboanugwo Collins <munisco12@gmail.com>
2020-09-29 18:03:29 +0100

    Building the homes section

--- src/components/Homes.tsx
+++ src/components/Homes.tsx
@@ -3,13 +3,30 @@ import React from 'react'
 function Homes() {
     return (
         <>
-            <div className="home">home 1</div>
+            <div className="home">
+                <img src='' alt=''/>
+                <div className="home__like"></div>
+                <h5 className="home__name">Beautiful Family House</h5>
+                <div className="home__locatin">
+                    <p>USA</p>
+                </div>
+                <div className="home__rooms">
+                    <p>5 rooms</p>
+                </div>
+                <div className="home__areas">
+                    <p>323 m<sup>2 </sup></p>
+                </div>
+                <div className="home__price">
+                    <p>$1,300,421</p>
+                </div>
+                <button className="cta">Contact Realtor</button>
+            </div>
             <div className="home">home 2</div>
             <div className="home">home 3</div>
             <div className="home">home 4</div>
             <div className="home">home 5</div>
             <div className="home">home 6</div>
-        </>
+        </img>
     )
 }
 
