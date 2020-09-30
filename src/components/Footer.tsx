1888eaaa03197a58ec662a21674549c0ef813d39
Igboanugwo Collins <munisco12@gmail.com>
2020-09-30 14:26:59 +0100

    Working on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,38 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <nav className='nav'>
+            <li className='nav__item'>
+                <a href='#' className='nav__link'>
+                    Find your dream home
+                </a>
+            </li>
+            <li className='nav__item'>
+                <a href='#' className='nav__link'>
+                    Request proposal
+                </a>
+            </li>
+            <li className='nav__item'>
+                <a href='#' className='nav__link'>
+                    Download home planner
+                </a>
+            </li>
+            <li className='nav__item'>
+                <a href='#' className='nav__link'>
+                    Submit your property{' '}
+                </a>
+            </li>
+            <li className='nav__item'>
+                <a href='#' className='nav__link'>
+                    Contact us
+                </a>
+            </li>
+            <li className='nav__item'>
+                <a href='#' className='nav__link'>
+                    Come work with us
+                </a>
+            </li>
+        </nav>
     )
 }
 
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,11 +1,65 @@
 import React from 'react'
+import Gal1 from '../images/gal-1.jpeg'
+import Gal2 from '../images/gal-2.jpeg'
+import Gal3 from '../images/gal-3.jpeg'
+import Gal4 from '../images/gal-4.jpeg'
+import Gal5 from '../images/gal-5.jpeg'
+import Gal6 from '../images/gal-6.jpeg'
+import Gal7 from '../images/gal-7.jpeg'
+import Gal8 from '../images/gal-8.jpeg'
+import Gal9 from '../images/gal-9.jpeg'
+import Gal10 from '../images/gal-10.jpeg'
+import Gal11 from '../images/gal-11.jpeg'
+import Gal12 from '../images/gal-12.jpeg'
+import Gal13 from '../images/gal-13.jpeg'
+import Gal14 from '../images/gal-14.jpeg'
 
 function Gallery() {
-    return (
-        <div>
-            Gallery
-        </div>
-    )
+    return <>
+
+        <figure className="gallery__figure-1">
+            <img src={Gal1} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-2">
+            <img src={Gal2} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-3">
+            <img src={Gal3} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-4">
+            <img src={Gal4} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-5">
+            <img src={Gal6} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-6">
+            <img src={Gal7} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-7">
+            <img src={Gal8} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-8">
+            <img src={Gal9} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-9">
+            <img src={Gal10} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-10">
+            <img src={Gal11} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-11">
+            <img src={Gal12} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-12">
+            <img src={Gal13} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-13">
+            <img src={Gal14} alt="" className='gallery__item' />
+        </figure>
+        <figure className="gallery__figure-14">
+            <img src={Gal5} alt="" className='gallery__item' />
+        </figure>
+    </>
 }
 
 export default Gallery
