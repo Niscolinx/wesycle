85ff2df4af8d0cb9804e9fb1a7531b01f1b58897
Igboanugwo Collins <munisco12@gmail.com>
2020-10-02 07:51:05 +0100

    workion on the header logos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,15 +9,15 @@ function Header() {
     return (
         <>
             <img src={Logo} alt='' className='header__logo' />
-            <h3 className='heading-3'>Your own home</h3>
+            <h3 className='header__text'>Your own home</h3>
             <h1 className='heading-1'>The Ultimate personal freedom</h1>
             <button className='btn header__btn'>View our properties</button>
             <div className='header__seen-on'>As seen on</div>
-            <div className='header__seen-on--logos'>
-                <img src={BBC} alt='' className='header__seen-on--item' />
-                <img src={Forbes} alt='' className='header__seen-on--item' />
-                <img src={Techcrunch} alt='' className='header__seen-on--item' />
-                <img src={BusinessInsider} alt='' className='header__seen-on--item' />
+            <div className='header__logos'>
+                <img src={BBC} alt='' className='header__item' />
+                <img src={Forbes} alt='' className='header__item' />
+                <img src={Techcrunch} alt='' className='header__item' />
+                <img src={BusinessInsider} alt='' className='header__item' />
             </div>
         </>
     )
