64d99b49579e29c368233bd7d5387d344495cd44
Igboanugwo Collins <munisco12@gmail.com>
2020-10-02 16:04:20 +0100

    Adding media queries

--- src/components/Realtors.tsx
+++ src/components/Realtors.tsx
@@ -6,21 +6,26 @@ import Realtor3 from '../images/realtor-3.jpeg'
 function Realtors() {
     return (
         <>
-            <h3 className="heading-3 realtors__heading">Top 3 Realtors</h3>
-            <div className="realtor">
-                <img src={Realtor1} alt="" className='realtor__img'/>
-                <h4 className="heading-4 realtor__name">Erik Fienman</h4>
-                <p className="realtor__house">245 Houses sold</p>
-            </div>
-            <div className="realtor">
-                <img src={Realtor2} alt="" className='realtor__img'/>
-                <h4 className="heading-4 realtor__name">Frank Richard</h4>
-                <p className="realtor__house">215 Houses sold</p>
-            </div>
-            <div className="realtor">
-                <img src={Realtor3} alt="" className='realtor__img'/>
-                <h4 className="heading-4 realtor__name">Tom Ramsey</h4>
-                <p className="realtor__house">184 Houses sold</p>
+            <h3 className='heading-3 realtors__heading'>Top 3 Realtors</h3>
+            <div className='realtor__list'>
+
+                <img src={Realtor1} alt='' className='realtor__img' />
+                <div className='realtor__details'>
+                    <h4 className='heading-4 realtor__name'>Erik Fienman</h4>
+                    <p className='realtor__house'>245 Houses sold</p>
+                </div>
+
+                <img src={Realtor2} alt='' className='realtor__img' />
+                <div className='realtor__details'>
+                    <h4 className='heading-4 realtor__name'>Frank Richard</h4>
+                    <p className='realtor__house'>215 Houses sold</p>
+                </div>
+
+                <img src={Realtor3} alt='' className='realtor__img' />
+                <div className='realtor__details'>
+                    <h4 className='heading-4 realtor__name'>Tom Ramsey</h4>
+                    <p className='realtor__house'>184 Houses sold</p>
+                </div>
             </div>
         </>
     )
