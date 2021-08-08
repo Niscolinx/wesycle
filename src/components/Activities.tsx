6b9941e7fa1149e41c16adcbb4062b0ca70c2e1e
Igboanugwo Collins <munisco12@gmail.com>
2021-08-08 19:53:06 +0100

    updated activities

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -20,7 +20,7 @@ function Features() {
                 <h4 className='heading-4 heading-4__dark'>World best luxury homes</h4>
                 <p className='feature__text'>
                     {' '}
-                    saepe ratione rem, in the place of good
+                    saepe ratione rem, in the place
                 </p>
             </div>
             <div className='feature'>
